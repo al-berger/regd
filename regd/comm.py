ac73700adecd483f1ca88f725351d74ab4e695de
Albert Berger <alberger@gmail.com>
2015-09-14 04:34:50 +0300

    Added default parameter to RegdComm.getToken

--- regd/comm.py
+++ regd/comm.py
@@ -10,7 +10,7 @@
 *		
 *********************************************************************/
 '''
-__lastedited__="2015-08-08 18:08:29"
+__lastedited__="2015-08-24 05:35:00"
 
 import os, subprocess as sp, time
 import regd.defs as defs, regd.util as util, regd.cli as cli
@@ -163,7 +163,15 @@ class RegdComm:
 	
 	def getToken(self, nam, *args, **kwargs ):
 		m = { "cmd": defs.GET_TOKEN, "params": [nam] }
-		return self.sendCmd(m, args, kwargs)
+		if "default" not in kwargs:
+			return self.sendCmd(m, args, kwargs)
+		else:
+			defRet = kwargs["default"]
+			del kwargs["default"]
+			res, ret = self.sendCmd(m, args, kwargs)
+			if not res:
+				ret = defRet
+			return res, ret
 
 	def getTokenSec( self, nam, *args, **kwargs ):
 		m = { "cmd": defs.GET_TOKEN_SEC, "params": [nam] }
